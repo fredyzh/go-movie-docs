958a14cae6f6c1ff803d0c95ad3aae02f078575b
Feng Zhang <fredyzh@gmail.com>
2023-07-20 11:16:53 -0400

    init

---
+++ theme.config.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Movie Ticket</span>,
+  project: {
+    link: "https://github.com/fredyzh/go-movie-fiber-gorm/tree/master",
+  },
+  footer: {
+    text: "Movie Ticket Practice Project",
+  },
+};
+
+export default config;
